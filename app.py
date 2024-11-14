184b79f4c465cfec356d17eacbcb0a85e23d1f79
steveGau <a290772@gmail.com>
2024-11-15 01:26:39 +0800

    first commit

--- app.py
+++ app.py
@@ -29,8 +29,8 @@ def download_video(url):
         print(f'Executing command: {command}')
         
         # Run the command using subprocess
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
-        
+        # result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        result = subprocess.run([coder_path, url], capture_output=True, text=True)
         if result.returncode == 0:
             flash("Video downloaded successfully.", "success")
             # Print the directory where the video is downloaded
